7956b68758c7c65389ad017cfb5b96d30c06a966
waiting <1661926154@qq.com>
2022-05-09 20:07:29 +0800

    refactor(ali-oss): key of rules

--- packages/ali-oss/src/lib/rule.ts
+++ packages/ali-oss/src/lib/rule.ts
@@ -2,7 +2,7 @@ import { DataKey } from './types'
 
 
 export const rules = {
-  elapsed: /(\d+\.\d+)\(s\)\s+elapsed(?=\s|\n|\r\n|$)/u,
+  [DataKey.elapsed]: /(\d+\.\d+)\(s\)\s+elapsed(?=\s|\n|\r\n|$)/u,
 }
 
 export const PickData = {
@@ -12,7 +12,7 @@ export const PickData = {
 export function pickElapsed(input: string, debug = false): string | undefined {
   // '0.312022(s) elapsed\n'
   debug && console.log({ pickElapsedInput: input })
-  const found = input.match(rules.elapsed)
+  const found = input.match(rules[DataKey.elapsed])
   if (found && found.length >= 1) {
     debug && console.log({ pickElapsedFound: found })
     return found[1]
