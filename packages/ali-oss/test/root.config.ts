47c2278de8be1a6515f5af4c01a5ead5a57915bf
waiting <1661926154@qq.com>
2022-05-09 16:30:21 +0800

    test(ali-oss): update root.config.ts

--- packages/ali-oss/test/root.config.ts
+++ packages/ali-oss/test/root.config.ts
@@ -7,19 +7,23 @@ export const endpoint = process.env.ALI_OSS_ENDPOINT ?? ''
 export const accessKeyId = process.env.ALI_OSS_ID ?? ''
 export const accessKeySecret = process.env.ALI_OSS_SECRET ?? ''
 export const bucket = process.env.ALI_OSS_BUCKET ?? ''
+export const CI = process.env.CI
 
-assert(endpoint, 'ALI_OSS_ENDPOINT is required')
-assert(accessKeyId, 'ALI_OSS_ID is required')
-assert(accessKeySecret, 'ALI_OSS_SECRET is required')
 assert(bucket, 'ALI_OSS_BUCKET is required')
-
 export const pathPrefix = `oss://${bucket}/mobileFile`
 
-const config: Config = {
-  // language: 'EN',
-  endpoint,
-  accessKeyId,
-  accessKeySecret,
+let config: Config | undefined = void 0
+if (CI) {
+  assert(endpoint, 'ALI_OSS_ENDPOINT is required')
+  assert(accessKeyId, 'ALI_OSS_ID is required')
+  assert(accessKeySecret, 'ALI_OSS_SECRET is required')
+  config = {
+    // language: 'EN',
+    endpoint,
+    accessKeyId,
+    accessKeySecret,
+  }
 }
+
 export const service = new OSSService(config)
 
