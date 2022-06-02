fd6eb222b27657061aa0793ce3a3eaf425199339
waiting <1661926154@qq.com>
2022-06-02 11:12:40 +0800

    test(ali-oss): update config

--- packages/ali-oss/test/root.config.ts
+++ packages/ali-oss/test/root.config.ts
@@ -17,7 +17,7 @@ export const bucket = process.env.ALI_OSS_BUCKET ?? ''
 export const CI = process.env.CI
 
 assert(bucket, 'ALI_OSS_BUCKET is required')
-export const cloudUrlPrefix = 'mobileFile/debug'
+export const cloudUrlPrefix = 'mobileFile/debug-' + Math.random().toString()
 
 export const configPath = join(homedir(), '.ossutilconfig')
 
