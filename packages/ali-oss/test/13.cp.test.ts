87fb12c84949c2cf8e42776b89390e44aac10fa1
waiting <1661926154@qq.com>
2022-06-02 19:45:52 +0800

    test(ali-oss): update test/13.cp.test.ts

--- packages/ali-oss/test/13.cp.test.ts
+++ packages/ali-oss/test/13.cp.test.ts
@@ -19,11 +19,11 @@ const __dirname = genCurrentDirname(import.meta.url)
 describe(fileShortPath(import.meta.url), () => {
 
   describe('cp should work', () => {
-    it('pass config file', async () => {
+    it('normal', async () => {
       assert(client2, 'client2 should be defined')
 
       const src = join(__dirname, 'tsconfig.json')
-      const target = `${cloudUrlPrefix}/${Date.now().toString()}-tsconfig.json`
+      const target = `${cloudUrlPrefix}/cp-${Date.now().toString()}-tsconfig.json`
 
       const opts: CpOptions = {
         bucket,
@@ -33,9 +33,17 @@ describe(fileShortPath(import.meta.url), () => {
       const ret = await client2.cp(opts)
       CI || console.log(ret)
       assert(! ret.exitCode, `cp ${src} ${target} failed, ${ret.stderr}`)
-      assert(ret.data)
-      assert(typeof ret.data.elapsed === 'string')
-      assert(typeof ret.data.averageSpeed === 'number')
+
+      const { data } = ret
+      assert(data)
+      assert(typeof data.elapsed === 'string', ret.stderr)
+      assert(typeof data.averageSpeed === 'number', ret.stderr)
+      assert(typeof data.succeedTotalNumber === 'number', ret.stderr)
+      assert(data.succeedTotalNumber === 1, ret.stderr)
+      assert(typeof data.succeedTotalSize === 'string', ret.stderr)
+      assert(data.succeedTotalSize.length, ret.stderr)
+      assert(typeof data.uploadDirs === 'undefined', ret.stderr)
+      assert(data.uploadFiles === 1)
     })
 
   })
